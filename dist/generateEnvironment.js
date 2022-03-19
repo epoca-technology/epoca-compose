ba037058c29e03664608b2d1703657f1b22672c2
JessDotJS <jessdotjs@gmail.com>
2022-03-19 19:14:59 -0400

    Candlestick spreadsheet completed

--- dist/generateEnvironment.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var Environment_1=require("./Environment"),_environment=new Environment_1.Environment;function main(){var n=_environment.generateEnvironment();console.log(" "),console.log(n),console.log(" ")}console.log(" "),console.log("GENERATE ENVIRONMENT"),console.log(" "),main();
